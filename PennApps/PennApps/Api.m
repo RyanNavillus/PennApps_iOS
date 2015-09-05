beae6e7f151d925fa0ae401a1cd6d6fc1f62e525
Ryan Sullivan <rps214@verizon.net>
2015-09-05 08:19:49 -0400

    New Api calls

--- PennApps/PennApps/Api.m
+++ PennApps/PennApps/Api.m
@@ -44,18 +44,51 @@ static Api* kSharedApi;
     
 }
 
--(void)postLoginInfoWithUserName:(NSString *)username andPassword:(NSString *)password{
+-(void)loginWithUserName:(NSString *)username andPassword:(NSString *)password{
     // 1
-    NSURL *url = [NSURL URLWithString:@"/login"];
+    NSURL *dataUrl = [NSURL URLWithString:@"/login"];
+    NSURL *url = [NSURL URLWithString: [NSString stringWithFormat:@"%@%@", kBaseURLString, dataUrl]];
+
     NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
     
     // 2
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
-    request.HTTPMethod = @"GET";
+    request.HTTPMethod = @"POST";
     
     // 3
     NSDictionary *dictionary = @{@"username": username, @"password" : password};
+    NSError *error = nil;
+    NSData *data = [NSJSONSerialization dataWithJSONObject:dictionary
+                                                   options:kNilOptions error:&error];
+    
+    if (!error) {
+        // 4
+        NSURLSessionUploadTask *uploadTask = [session uploadTaskWithRequest:request
+                                                                   fromData:data completionHandler:^(NSData *data,NSURLResponse *response,NSError *error) {
+                                                                       NSString *myString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                                                                       NSLog(@"%@", myString);
+                                                                   }];
+        
+        // 5
+        [uploadTask resume];
+    }
+    
+}
+
+-(void)registerWithUserName:(NSString *)username Password:(NSString *)password Specialty:(NSString *)specialty Name:(NSString *)name{
+    // 1
+    NSURL *dataUrl = [NSURL URLWithString:@"/register"];
+    NSURL *url = [NSURL URLWithString: [NSString stringWithFormat:@"%@%@", kBaseURLString, dataUrl]];
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
+    
+    // 2
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:url];
+    request.HTTPMethod = @"POST";
+    
+    // 3
+    NSDictionary *dictionary = @{@"username": username, @"password" : password, @"specialty" : specialty, @"name" : name};
     NSError *error = nil;
     NSData *data = [NSJSONSerialization dataWithJSONObject:dictionary
                                                    options:kNilOptions error:&error];
