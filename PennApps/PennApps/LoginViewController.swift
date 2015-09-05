94bf99040eaa4ccd63902d62a1e5a0094c6bc3fb
Darpan Shah <darpanshah@Darpans-MacBook-Pro.local>
2015-09-04 22:41:40 -0400

    added registration code

--- PennApps/PennApps/LoginViewController.swift
+++ PennApps/PennApps/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class LoginViewController {
     
@@ -85,5 +86,68 @@ class LoginViewController {
         }
         
     }// end request
+    
+    
+    
+    // Register method that sends request to server with user information
+    //
+    func regiserUser(firstName: String, lastName: String, email: String, password: String, birthDate: String, height: Float, gender: String, completionHandler : ((isResponse : Bool) -> Void)){
+        println()
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        configuration.timeoutIntervalForResource = 4 // seconds
+        
+        self.alamofireManager = Alamofire.Manager(configuration: configuration)
+        self.alamofireManager!.request(.POST, "URL of the server goes here", parameters: [
+            "email":email,
+            "password": password,
+            "first_name" : firstName,
+            "last_name" : lastName,
+            "gender" : gender,
+            ]).responseJSON() { (request, response, data, error) in
+                
+                println(error)
+                println(response)
+                println(data)
+                
+                if data == nil {
+                    completionHandler(isResponse: false)
+                    self.errors = "Server ERROR"
+                    User().alert("Server Error", message: "Server missing")
+                    
+                }else{
+                    var json = JSON(data!)
+                    println("json and data \(data!)")
+                    // convert JSON key into string
+                    var e = json["email"]
+                    var serverEmail = "\(e)"
+                    var serverReply = json["email"][0]
+                    println(serverEmail)
+                    // Ensure the response email mathches the input and log the user in
+                    //
+                    if serverEmail == email{
+                        var successfulLogin: () = self.loginUserWithEmailAndPassword(email, password: password, remember: false, completionHandler: { (isResponse) -> Void in
+                            
+                            if isResponse{
+                                completionHandler(isResponse: true)
+                            } else {
+                                completionHandler(isResponse: false)
+                            }
+                            
+                        })
+                    }else if serverReply == "This field must be unique."{
+                        
+                        User().alert("Existing User", message: "User Exists already!")
+                    }
+                    else if serverReply == "Enter a valid email address."{
+                        User().alert("Invalid Email", message: "Enter a valid email address.")
+                    }
+                    else{
+                        println("Nothing I can do")
+                    }
+                }
+                
+        } // end of response
+    } // end of register
+    
     
 }
