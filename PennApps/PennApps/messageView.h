c96a298306f860f74b0b36c1a0d6f589be3a4097
Ryan Sullivan <rps214@verizon.net>
2015-09-06 00:38:10 -0400

    commit

---
+++ PennApps/PennApps/messageView.h
@@ -0,0 +1,13 @@
+//
+//  messageView.h
+//  PennApps
+//
+//  Created by Ryan Sullivan on 9/6/15.
+//  Copyright © 2015 Ryan Sullivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface messageView : UIView
+
+@end
